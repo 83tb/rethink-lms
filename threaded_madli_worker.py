84a07a77e101786576fd6fdb01d6d3ab34031ee9
kuba kucharski <kuba@kucharski.it>
2015-06-17 18:17:09 +0200

    threaded

--- threaded_madli_worker.py
+++ threaded_madli_worker.py
@@ -5,10 +5,9 @@ from madli import *
 import rethinkdb as r
 conn = r.connect("localhost").repl()
 
-lamps_table = r.db("engine").table("lamps")
-
+db = r.db("engine")
+lamps_table = db.table("lamps")
 cursor = lamps_table.changes().run(conn)
-
 
 import logging
 import threading
@@ -24,30 +23,47 @@ logging.basicConfig(level=logging.DEBUG,
 
 def quick_commands():
     for feed in cursor:
+        logging.debug('New change in Rethink DB detected!')
         with lock:
+            logging.debug(feed)
             old_lamp = feed['old_val']
             lamp = feed['new_val']
+                
+            lamp['scheduled_read'] = True
+            if lamp['scheduled_read']:
+                logging.debug('Read scheduled detected')
 
-            schedule_read = True
-            if schedule_read:
-                slow_reads.append(dict(command="Read"))
-
-            if old_lamp['wanted_l_level'] != lamp['wanted_l_level']:
-                if lamp['wanted_l_level'] == 0:
-                    Off(lamp['hardware']['address'], lamp['wanted_l_level'])
-                else:
-                    setDim(lamp['hardware']['address'], lamp['wanted_l_level'])
-
-
+                slow_reads.append(dict(command="GetRam", lampNumber=lamp['hardware']['address'], address=1, id=lamp['id']))    
+                lamp['scheduled_read'] = False
+                lamp['change_required'] = False
+                
+                lamps_table.update(lamp).run(conn)            
+            if old_lamp['special_l_setting'] != lamp['special_l_setting']:
+                logging.debug('User wants to change the lightning level!')
+ 
+                if old_lamp['special_l_setting'] != lamp['special_l_setting']:
+                    lamp['wanted_l_level'] = lamp['special_l_setting']
+                
+import time
 def slow_commands():
     while True:
-        if not lock.locked():
-            with lock:
+        time.sleep(10)
+        with lock:
+            if slow_reads:
                 task = slow_reads.pop()
                 if task:
-                    # execute task
-                    # write to rethink
-                    pass
+                    try:
+                        actual_driver_value = readValue(task['command'], task['lampNumber'], task['address']).get('data1')
+                        lamp = dict(id=task['id'], actual_driver_value=actual_driver_value)
+                        lamp['change_required'] = False
+
+                        lamps_table.update(lamp).run(conn)            
+
+                        print actual_driver_value
+                    except Exception(), e:
+                        print e
+                        
+                                        
 
 
 def writes():
