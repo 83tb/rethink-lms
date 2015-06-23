b725f9a371ea630db6cc175b2b7d07ed58136f7e
Kuba Kucharski <kuba@growbots.co>
2015-06-23 22:50:58 +0200

    fix in table creation flow

--- engine.py
+++ engine.py
@@ -21,15 +21,16 @@ def setup_db(db_name="engine", tables=['lamps','settings','commands' ]):
         r.db_create(db_name).run(connection)
     except r.RqlRuntimeError:
         logging.warn('Database already exists.')
-    for tbl in tables:
-        try:
-            r.db(db_name).table_create(tbl, durability="hard").run(connection)
-        except r.RqlRuntimeError:
-            logging.warn('Table already exists.')
-    logging.info('Database setup completed.')
+
 
 
     else:
+        for tbl in tables:
+            try:
+                r.db(db_name).table_create(tbl, durability="hard").run(connection)
+            except r.RqlRuntimeError:
+                logging.warn('Table already exists.')
+        logging.info('Database setup completed.')
         r.db(db_name).table('lamps').index_create('location', geo=True).run(connection)
     finally:
         connection.close()
