dac56a9f133e1cfc9bae245e2b0c6b07ab7c11f5
mikostn <miko-git@stn.pl>
2016-09-16 09:27:19 +0200

    add xy coords

--- scripts/data_import_csv.py
+++ scripts/data_import_csv.py
@@ -13,7 +13,8 @@ def setup_db(db_name="engine", tables=['lamps', ]):
         for tbl in tables:
             r.db(db_name).table_create(tbl, durability="hard").run(connection)
         logging.info('Database setup completed.')
-        # r.db(db_name).table('lamps').index_create('location', geo=True).run(connection)
+        # r.db(db_name).table(
+        #    'lamps').index_create('location', geo=True).run(connection)
 
     except r.RqlRuntimeError:
         logging.warn('Database/Table already exists.')
@@ -31,7 +32,7 @@ except r.RqlRuntimeError:
     logging.warn('Index already exists.')
 
 
-def returnLampObj(id, coords):
+def returnLampObj(id, coords, xy):
     location = {
         "type": "Point",
         "coordinates": coords
@@ -60,34 +61,40 @@ def returnLampObj(id, coords):
         "working_flag": "false",
         "change_required": "true",
         "location": geo_position,
+        "xy": xy
     }
     return lamp
 
 
 db_lamps.delete().run(c)
 
-with open('lamps.csv', 'rb') as csvfile:
+with open('../local/lamps.csv', 'rb') as csvfile:
     base_coords = [20.74764, 52.17831]
-    #jsonData = []
+    # jsonData = []
     iCY = 0
     i = 0
+    rowN = 0
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         coordY = base_coords[1] - iCY
         iCY += 0.0001
         iCX = 0
+        rowItem = 0
         for lid in row:
             coordX = base_coords[0] + iCX
             iCX += 0.0001
             coords = [coordX, coordY]
+            xy = [rowItem, rowN]
             if(lid):
                 i += 1
                 print "Lamp: " + str([i, lid, coords])
-#				jsonData.append(returnLampObj(lid, coords))
-                db_lamps.insert(returnLampObj(lid, coords)).run(c)
+                # jsonData.append(returnLampObj(lid, coords))
+                db_lamps.insert(returnLampObj(lid, coords, xy)).run(c)
+            rowItem += 1
+        rowN += 1
 print "Total " + str(i) + " lamps"
 # print jsonData
 
 # with open('universal-lamps.json', 'w') as outfile:
-#	json.dump(jsonData, outfile, sort_keys=True, indent=4,
-# ensure_ascii=False)
+#     json.dump(
+#         jsonData, outfile, sort_keys=True, indent=4, ensure_ascii=False)
